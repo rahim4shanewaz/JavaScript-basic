b8f7759c85ce3006485728d75741f67b633f516d
rahim4shanewaz <soumiq444@gmail.com>
2022-08-02 00:41:34 +0600

    solve

---
+++ problem-1.js
@@ -0,0 +1,26 @@
+var a = 80;
+var b = 60;
+var c = 50;
+var d = 40;
+var myScore = 85;
+
+ 
+
+if(myScore >= a ){
+    console.log("my Grade is A");
+}
+ else if (myScore >= b) {
+    console.log("my Grade is B");
+    
+ }
+ else if (myScore >= c) {
+    console.log("my Grade is C");
+    
+ }
+ else if (myScore >= d) {
+    console.log("my Grade is D");
+    
+ }
+ else {
+    console.log("Fail");
+ }
---
+++ problem-2.js
@@ -0,0 +1,12 @@
+var num1 = 103;
+var num2 = 79;
+var num3 = 195;
+if(num1>num2 && num1>num3){
+    console.log("num1 is largest number");
+}
+else if(num2>num1 && num2>num3){
+    console.log("num2 is largest number");
+}
+else{
+    console.log("num3 is largest number");
+}
