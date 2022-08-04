85f290213fd6a64483c91837ba826b148a821b5d
rahim4shanewaz <soumiq444@gmail.com>
2022-08-04 21:42:32 +0600

    practise

---
+++ problem-3.js
@@ -0,0 +1,20 @@
+var i = 1;
+for (i < 5; i++) {
+  console.log(i);
+}
+var myMoney = 50000;
+ if( myMoney >=800000){
+    console.log('i will buy mac');
+ }
+ else if(myMoney>=60000){
+    console.log('i will buy gaming laptop');
+ }
+ else if(myMoney>=40000){
+    console.log('i will buy lenovo laptop');
+ }
+ else if(myMoney>=20000){
+    console.log('i will buy old laptop');
+ }
+ else{
+    console.log('i am ok with mobile');
+ }
---
+++ random-problem.js
@@ -0,0 +1,43 @@
+ /* var bookPrice = [134, 67, 87, 44, 34,87, 245, 345, 567, 89, 176, 197, 400];
+ for(var i=0; i<bookPrice.length; i++){
+    
+    if(bookPrice[i]  > 200){
+        continue;
+    }
+    console.log(bookPrice[i]); 
+ }*/
+
+ 
+/* for( var i = 30; i<=86; i++){
+    if(i===45)
+    break;
+    console.log(i);
+ }*/
+
+ /* var mobileName = ['nktel','symphony','nokia','samsung'];
+ for(var i=0; i<mobileName.length; i++){
+    var mobile = mobileName[i];
+    console.log("I used all these phone" + " " + mobile);
+ } */
+/* 
+ for(i=0; i<= 39; i+=1){
+    console.log(i);
+    console.log('Ajke amar mon valo ney');
+ }
+ */
+/*var myMoney = 1000000;
+ if( myMoney >=800000){
+    console.log('i will buy mac');
+ }
+ else if(myMoney>=60000){
+    console.log('i will buy gaming laptop');
+ }
+ else if(myMoney>=40000){
+    console.log('i will buy lenovo laptop');
+ }
+ else if(myMoney>=20000){
+    console.log('i will buy old laptop');
+ }
+ else{
+    console.log('i am ok with mobile');
+ } */
